48b40b6570608e56386b870433c8c3f741eff692
sqrt <sqrt@octoblu.com>
2015-09-18 11:15:44 -0700

    Updated fix-config

--- fix-config.sh
+++ fix-config.sh
@@ -3,15 +3,27 @@
 ### UPDATE ROUTER
 
 FLOW_ID=dd3d787a-7833-4581-9287-3ad2c5a1273a
-NODE_ID=router
-INSTANCE_ID=fcdd26a0-5d65-11e5-9fc2-63ce49df012b
+INSTANCE_ID=0e505050-5e2a-11e5-a29d-99c2e61be901
 
-KEY=$FLOW_ID/$INSTANCE_ID/$NODE_ID/config
+# Update engine-output
+KEY="$FLOW_ID/$INSTANCE_ID/engine-output/config"
+redis-cli SET $KEY "$(redis-cli GET $KEY | jq 'del(.host)')"
 
-ROUTER_CONFIG=$(redis-cli GET $KEY | jq 'setpath(["meshblu-output"]; getpath(["output"]))' | jq 'setpath(["meshblu-output", "type"]; "meshblu-output")')
+# Update engine-debug
+KEY="$FLOW_ID/$INSTANCE_ID/engine-debug/config"
+DEBUG_CONFIG=$(redis-cli GET $KEY | sed -e 's/nodeUuid/nodeId/g')
+redis-cli SET $KEY "$DEBUG_CONFIG"
+
+# Update engine-pulse
+KEY="$FLOW_ID/$INSTANCE_ID/engine-pulse/config"
+PULSE_CONFIG=$(redis-cli GET $KEY | sed -e 's/nodeUuid/nodeId/g')
+redis-cli SET $KEY "$PULSE_CONFIG"
+
+# Update router
+KEY="$FLOW_ID/$INSTANCE_ID/router/config"
+DEBUG_LINKTO='["engine-debug","engine-pulse"]'
+DEBUG_ID="106b1a01-5e2a-11e5-a29d-99c2e61be901"
+ROUTER_CONFIG=$(redis-cli GET $KEY | jq "setpath([\"$DEBUG_ID\",\"linkedTo\"]; $DEBUG_LINKTO)")
 redis-cli SET $KEY "$ROUTER_CONFIG"
 
 
-### COPY OUTPUT NODE
-
-redis-cli SET $FLOW_ID/$INSTANCE_ID/meshblu-output/config '{ "uuid": "dd3d787a-7833-4581-9287-3ad2c5a1273a", "token": "b24285d321c65758f4b43c34fd494467b4b8622e", "server": "meshblu.octoblu.com", "port": 443 }'
