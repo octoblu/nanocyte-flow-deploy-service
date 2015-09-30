2b6e6923a7469c4a19565aa4e4f898af756af81a
sqrt <sqrt@octoblu.com>
2015-09-30 16:56:25 -0700

    v1.14.1 use latest, that actually works

--- deploy-flow.sh
+++ deploy-flow.sh
@@ -2,10 +2,10 @@
 # meshbluAuth:
 #   uuid: 'user-uuid'
 #   token: 'user-token
-USER_UUID="af354bd1-988f-4905-bba7-98b87f84eabe"
-USER_TOKEN="b52d705520c9adf5e64afce81dc0d538095f2b09"
+USER_UUID="4e565c47-e80f-44b3-b2da-9fe23bb60222"
+USER_TOKEN="886d3621c1b92bae60f55d1847888b84afe1348c"
 # FLOW_UUID='dd3d787a-7833-4581-9287-3ad2c5a1273a' # Bonzai
-FLOW_UUID='c36f335a-d820-42bc-bedb-b08775931318' # Demultiplex Test
+FLOW_UUID='6707e56b-7070-41d9-b706-7cf6fc7bbeb6' # Demultiplex Test
 
 #URL="http://$USER_UUID:$USER_TOKEN@nanocyte-flow-deploy.octoblu.com/flows/$FLOW_UUID/instances"
 URL="http://$USER_UUID:$USER_TOKEN@localhost:5051/flows/$FLOW_UUID/instances"
