ca2e218245b2d5a42d79a7066417372659a6f6e0
Jade Meskill <jade.meskill@gmail.com>
2016-02-18 11:19:13 -0700

    v1.23.4 production dockerfile, move healthcheck, add sigterm handler

--- get-octoblu-user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-USER_UUID="af354bd1-988f-4905-bba7-98b87f84eabe"
-USER_TOKEN="b52d705520c9adf5e64afce81dc0d538095f2b09"
-FLOW_UUID='c36f335a-d820-42bc-bedb-b08775931318' # Demultiplex Test
-curl "https://$USER_UUID:$USER_TOKEN@api.octoblu.com/api/user"
